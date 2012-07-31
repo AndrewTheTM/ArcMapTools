024996c4ac3db5e107207a99b1015bda31f645af
Andrew Rohne <arohne@oki.org>
2012-07-31 09:35:37 -0400

    Completed definition query button.  Started work on a buttons to flip the boarding and alighting AND origin and destination locations

--- src/org/oki/transmodel/arcgisAddIns/flipBoardAlight.java
+++ src/org/oki/transmodel/arcgisAddIns/flipBoardAlight.java
@@ -1,14 +1,14 @@
 package org.oki.transmodel.arcgisAddIns;
 
 import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 import com.esri.arcgis.addins.desktop.Button;
 import com.esri.arcgis.arcmapui.IMxDocument;
 import com.esri.arcgis.carto.FeatureLayer;
-import com.esri.arcgis.carto.IFeatureLayerDefinition;
 import com.esri.arcgis.carto.IFeatureSelection;
-import com.esri.arcgis.carto.ILayer;
 import com.esri.arcgis.carto.IMap;
-import com.esri.arcgis.carto.Map;
 import com.esri.arcgis.framework.IApplication;
 import com.esri.arcgis.geodatabase.IEnumIDs;
 import com.esri.arcgis.geodatabase.IRow;
@@ -16,10 +16,11 @@ import com.esri.arcgis.geodatabase.ISelectionSet;
 import com.esri.arcgis.geodatabase.ITable;
 import com.esri.arcgis.interop.AutomationException;
 
-public class defQueryButton extends Button {
+public class flipBoardAlight extends Button{
 	private IApplication app;
+	@SuppressWarnings("unused")
 	@Override
-	public void onClick() throws IOException, AutomationException {
+	public void onClick() throws IOException, AutomationException{
 		try{
 			IMxDocument mxDoc = new com.esri.arcgis.arcmapui.IMxDocumentProxy (app.getDocument());
 			IMap focusMap = mxDoc.getFocusMap();
@@ -32,38 +33,38 @@ public class defQueryButton extends Button {
 						IFeatureSelection featSel=layer;
 						ISelectionSet selSet=featSel.getSelectionSet();
 						ITable table=selSet.getTarget();
-						int snFieldN=table.findField("SAMPN");
+						
+						int boardXField=table.findField("BX");
+						int boardYField=table.findField("BY_");
+						int alightXField=table.findField("AX");
+						int alightYField=table.findField("AY");
+														
 						IEnumIDs selIds=selSet.getIDs();
 						int iId=selIds.next();
 						int iaId[]=new int[selSet.getCount()];
+						if(selSet.getCount()>1){
+							int reply=JOptionPane.showConfirmDialog(null, "This will flip the boarding/alighting locations for ALL SELECTED items.  Is this what you want?","Confirm",JOptionPane.YES_NO_OPTION);
+							if(reply==JOptionPane.NO_OPTION){
+								System.exit(0); //<~ NOT the right command to use!  Exits ArcMap!
+							}
+						}
+						/*
 						int a=0;
 						while(iId>0){
 							IRow row=table.getRow(iId);
 							iaId[a]=((Number)row.getValue(snFieldN)).intValue(); //<~~ java.lang.Double cannot be cast to java.lang.Integer
 							a++;
 							iId=selIds.next();
-						}
-						for(int b=0;b<iaId.length;b++){
-							sqlString+="\"SAMPN\"="+iaId[b];
-						}
+						}*/
+						
 					}
 				}
 			}
-			for(int x=0;x<focusMap.getLayerCount();x++){
-				if(focusMap.getLayer(x).getName().equals("Destination Locations") || 
-						focusMap.getLayer(x).getName().equals("Alighting Locations") || 
-						focusMap.getLayer(x).getName().equals("Boarding Locations")){
-					ILayer layer=focusMap.getLayer(x);
-					IFeatureLayerDefinition ld=new com.esri.arcgis.carto.IFeatureLayerDefinitionProxy (layer);
-					ld.setDefinitionExpression(sqlString);
-				}
-			}
-			Map focusMap2=(Map) focusMap;
-			focusMap2.refresh();
 		}
 		catch(Exception e){
-			System.out.println(e.getMessage());
-		}
+							System.out.println(e.getMessage());
+		}			
+						
 	}
 	@Override
 	public void init(IApplication app) throws IOException, AutomationException {
